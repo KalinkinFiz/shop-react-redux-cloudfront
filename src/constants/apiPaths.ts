a666ffd839bd34b588e6c55338f0f954a9124d1d
KalinkinFiz <kalinkin.fiz@gmail.com>
2021-09-23 01:26:48 +0300

    feat: solution task

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://nxpj5h89uf.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://nxpj5h89uf.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://1ii8dz2dw7.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://nxpj5h89uf.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://nxpj5h89uf.execute-api.eu-west-1.amazonaws.com/dev",
 };
