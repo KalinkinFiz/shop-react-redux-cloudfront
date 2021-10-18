21fc708914eb51608a44d0421564b00c1fd19233
KalinkinFiz <kalinkin.fiz@gmail.com>
2021-10-19 02:22:21 +0300

    fix: localStorage

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://5vxe7qzza2.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://5vxe7qzza2.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://gc1pw0qifh.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://yyflmc8b2j.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://5vxe7qzza2.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://5vxe7qzza2.execute-api.eu-west-1.amazonaws.com/dev",
 };
