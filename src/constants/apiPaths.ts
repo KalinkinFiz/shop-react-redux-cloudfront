6c486e8b6e70b63679878461966541754a7dc39a
KalinkinFiz <kalinkin.fiz@gmail.com>
2021-10-24 17:18:07 +0300

    feat: add cart api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://5vxe7qzza2.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://yyflmc8b2j.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://5vxe7qzza2.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://5vxe7qzza2.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://rs-cart-api-dev.eu-west-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
